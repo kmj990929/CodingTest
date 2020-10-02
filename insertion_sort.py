efdbbd7de1837c988c5bf1829d41ea936040885b
repl.it user <kmj990929@dgist.ac.kr>
2020-10-02 14:34:32 +0000

    insertion_sort

---
+++ insertion_sort.py
@@ -0,0 +1,14 @@
+#insertion_sort
+#이것이 코딩테스트다 6.
+
+
+array = [7,5,9,0,3,1,6,2,4,8]
+
+for i in range(1,len(array)):
+  for j in range(i,0,-1):
+    if(array[j] < array[j-1]):
+      array[j], array[j-1] = array[j-1], array[j]
+    else:
+      break
+
+print(array)
